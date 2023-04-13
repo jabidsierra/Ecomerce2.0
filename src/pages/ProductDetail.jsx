9b6433319fbea617caf5ada023f8ee71e760f826
Jabid Sierra <jabidjunior@gmail.com>
2023-04-13 09:22:13 -0500

    arreglo final62

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -4,7 +4,6 @@ import { useParams } from 'react-router-dom'
 import { setIsLoading } from '../store/slices/isLoading.slice'
 import { useDispatch } from 'react-redux'
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
