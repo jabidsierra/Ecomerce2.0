c73863dc3b59e4e252ff2e8159cfa263d97ef8a6
Jabid Sierra <jabidjunior@gmail.com>
2023-04-13 06:56:09 -0500

    act1

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App';
 import store from './store';
 import { Provider } from 'react-redux';
 import './index.css'
-import "bootswatch/dist/slate/bootstrap.min.css";
+import "bootswatch/dist/morph/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
