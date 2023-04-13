3143ad658eead859ae8bbed12a72d5123e686c6e
Jabid Sierra <jabidjunior@gmail.com>
2023-04-13 14:06:57 -0500

    ultimo2.025

--- src/components/CartSidebar.jsx
+++ src/components/CartSidebar.jsx
@@ -16,8 +16,7 @@ const CartSidebar = ( { show, handleClose } ) => {
         <Offcanvas.Title>CART</Offcanvas.Title>
       </Offcanvas.Header>
       <Offcanvas.Body>
-        Some text as placeholder. In real life you can have the elements you
-        have chosen. Like, text, images, lists, etc.
+        
       </Offcanvas.Body>
     </Offcanvas>
   );
